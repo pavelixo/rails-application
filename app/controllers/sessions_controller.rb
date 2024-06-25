a8e74d2a0ccd8a1079f19d7ed57df6bba7342420
pavelixo <trashxindoragon87@gmail.com>
2024-06-24 21:25:22 -0300

    adding session and users controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Logged in successfully!'
+    else
+      flash.now[:alert] = 'Invalid username or password'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logged out successfully!'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  # Register
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  # Login
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy' # Logout
 
   root 'pages#index'
 end
