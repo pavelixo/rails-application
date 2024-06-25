a0b7f6a5826fd4a2a4f2f8e1926229f1a18d486e
pavelixo <trashxindoragon87@gmail.com>
2024-06-25 11:52:16 -0300

    adding sign up styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Register
-  get '/signup', to: 'users#new'
+  get '/signup', to: 'users#new', as: 'register'
   post '/signup', to: 'users#create'
 
   # Login
-  get '/signin', to: 'sessions#new'
+  get '/signin', to: 'sessions#new', as: 'login'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy' # Logout
 
