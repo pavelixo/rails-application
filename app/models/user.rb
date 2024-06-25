d3c44f598282a01b9e967b2a3341b641122baa99
pavelixo <trashxindoragon87@gmail.com>
2024-06-24 21:14:31 -0300

    users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true,
+            format: {
+              with: /\A[a-zA-Z0-9_]+\z/,
+              message: "only allows letters, numbers, and underscores"
+            }
+  validates :email, presence: true, uniqueness: true,
+            format: {
+              with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,
+              message: "must be a valid email address"
+            }
   validates :password, presence: true, length: { minimum: 6 }
 end
